202f48d0dd79d4046dd64da46cd88ca42af33741
Mohamed Faaris <mohamedfaaris.dev@gmail.com>
2026-03-10 01:28:25 +0530

    model

--- app/app/Models/Note.php
+++ app/app/Models/Note.php
@@ -9,7 +9,42 @@ class Note extends Model
     //
     use HasFactory;
 
-    protected $fillable
+    protected $fillable = [
+        'title',
+        'content',
+        'user_id',
+        'favorite_at',
+    ];
 
+    protected $casts = [
+    'favorited_at' => 'datetime',
+];
+    public function isFavorited()
+    {
+        return $this->favorited_at !== null;
+    }
+
+    public function favorite()
+    {
+        $this->favorited_at = now();
+        $this->save();
+    }
+
+    public function unfavorite()
+    {
+        $this->favorited_at = null;
+        $this->save();
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
+    function scopeFavorited($query)
+    {
+        return $query->whereNotNull('favorited_at');
+    }
 }
 
